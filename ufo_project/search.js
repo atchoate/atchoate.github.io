9fbd4c2fa6673c2f9df45ce0de8e4dbe4722a0eb
atchoate <43826834+atchoate@users.noreply.github.com>
2019-02-21 23:37:11 -0600

    Add files via upload

---
+++ ufo_project/search.js
@@ -0,0 +1,34 @@
+var UFOdata = data;
+var tbody = d3.select("tbody");
+
+var submit = d3.select("#submit")
+
+submit.on("click", function() {
+    d3.event.preventDefault();
+
+    var dateElement = d3.select("#date-search-input");
+
+    var dateValue = dateElement.property("value");
+
+    // console.log(dateValue);
+    // console.log(UFOdata);
+
+    var dateFilter = UFOdata.filter(encounter => encounter.datetime === dateValue);
+
+    //console.log(dateFilter)
+
+    document.getElementById("search_results").innerHTML="";
+
+    dateFilter.forEach(function(display) {
+        var row = tbody.append("tr");
+        Object.entries(display).forEach(function([key, value]) {
+            var cell = tbody.append("td");
+            cell.text(value);
+        })
+    })
+
+    
+
+
+
+})
